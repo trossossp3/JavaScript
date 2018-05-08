997367336aa85902592b9e2765d7edfca7c56e0d
Trent Rossos <31897216+trossossp3@users.noreply.github.com>
2018-05-08 09:36:32 -0400

    used cron with nodemailer

--- firstNode/emailNode.js
+++ firstNode/emailNode.js
@@ -1,4 +1,6 @@
 var nodemailer = require('nodemailer');
+var CronJob = require('cron').CronJob;
+var to = 'trossos@bayviewglen.ca';
 
 var transporter = nodemailer.createTransport({
 
@@ -12,21 +14,25 @@ var transporter = nodemailer.createTransport({
 
 var mailOptions = {
     from: 'nodejs72@gmail.com',
-    to: 'trossos@bayviewglen.ca',
+    to: to,
     subject: 'dw bout it',
-    html: '<p>this was an automated email from a server i made i was just trying it. if u got it inform me</p><h1>this is something i am trying</h1>',
-    attachments: [
+    html: '<h1><b1> Hello Commrad</h1></b1> <p>you have been selected for a free oportunity. fly to russia to get your prize</p>',
+    /*attachments: [
         {   // file on disk as an attachment
             filename: 'geoIsp2.docx',
             path: 'C:\\Users\\trossos\\Documents\\geoIsp2.docx' // stream this file
         }
-    ]
+    ]*/
 };
 
-transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Email sent: ' + info.response);
-    }
-}); 
+new CronJob('* * * * * *', function () {
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}, null, true, 'America/New_York');
+
