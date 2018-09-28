c8d37e4d8afa86c5dae21971f343eaadac2847fd
Trent Rossos <31897216+trossossp3@users.noreply.github.com>
2018-09-28 15:19:00 -0400

    first commit in a while

--- firstNode/firstSocket.js
+++ firstNode/firstSocket.js
@@ -1,21 +1,22 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var prompt = require('prompt-sync') ();
+var io = require("socket.io");
+var socket = io.listen(1223, "1.2.3.4");
+var people = {};
 
+socket.on("connection", function (client) {
+	client.on("join", function(name){
+		people[client.id] = name;
+		client.emit("update", "You have connected to the server.");
+		socket.sockets.emit("update", name + " has joined the server.")
+		socket.sockets.emit("update-people", people);
+	});
 
+	client.on("send", function(msg){
+		socket.sockets.emit("chat", people[client.id], msg);
+	});
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-
-
-io.on('connection', function (socket) {
-    socket.on('chat message', function (name, msg) {
-        io.emit('chat message', name, msg);
-    });
-});
-
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+	client.on("disconnect", function(){
+		socket.sockets.emit("update", people[client.id] + " has left the server.");
+		delete people[client.id];
+		socket.sockets.emit("update-people", people);
+	});
 });
